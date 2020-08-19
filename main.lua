d89a1de1274a3b2c06048da26a447a94df01f43d
David Konsumer <konsumer@jetboystudio.com>
2020-08-19 10:35:15 -0700

    basic triangle

--- main.lua
+++ main.lua
@@ -47,48 +47,19 @@ local top
 local sink
 
 function love.load()
+	local src = radio.SignalSource('triangle', 440, 44100)
 	sink = LoveAudioSinkMono()
-
-	local frequency = 91.5e6
-	local tune_offset = -250e3
-
-	local top = radio.CompositeBlock()
-	local source = radio.RtlSdrSource(frequency + tune_offset, 1102500)
-	local tuner = radio.TunerBlock(tune_offset, 200e3, 5)
-	local fm_demod = radio.FrequencyDiscriminatorBlock(1.25)
-	local hilbert = radio.HilbertTransformBlock(129)
-	local delay = radio.DelayBlock(129)
-	local pilot_filter = radio.ComplexBandpassFilterBlock(129, {18e3, 20e3})
-	local pilot_pll = radio.PLLBlock(100, 19e3-50, 19e3+50, 2)
-	local mixer = radio.MultiplyConjugateBlock()
-	local lpr_filter = radio.LowpassFilterBlock(128, 15e3)
-	local lpr_am_demod = radio.ComplexToRealBlock()
-	local lmr_filter = radio.LowpassFilterBlock(128, 15e3)
-	local lmr_am_demod = radio.ComplexToRealBlock()
-	local l_summer = radio.AddBlock()
-	local l_af_deemphasis = radio.FMDeemphasisFilterBlock(75e-6)
-	local l_downsampler = radio.DownsamplerBlock(5)
-	
-	top:connect(source, tuner, fm_demod, hilbert, delay)
-	top:connect(hilbert, pilot_filter, pilot_pll)
-	top:connect(delay, 'out', mixer, 'in1')
-	top:connect(pilot_pll, 'out', mixer, 'in2')
-	top:connect(delay, lpr_filter, lpr_am_demod)
-	top:connect(mixer, lmr_filter, lmr_am_demod)
-	top:connect(lpr_am_demod, 'out', l_summer, 'in1')
-	top:connect(lmr_am_demod, 'out', l_summer, 'in2')
-	top:connect(l_summer, l_af_deemphasis, l_downsampler)
-	top:connect(r_downsampler, 'out', sink, 'in')
-
+	top = radio.CompositeBlock()
+  top:connect(src, sink)
 	top:start()
 end
 
 function love.update(dt)
-	snk.update()
+	sink.update()
 end
 
 function love.quit()
 	top:stop()
-	snk:release()
+	sink:release()
 	top:wait()
 end
